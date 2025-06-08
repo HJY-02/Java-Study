2bb180a0ccc04f1390fd67ddcb2e69820bb61756
Hwangjunyeon <lambmi0207@naver.com>
2025-06-08 21:51:12 +0900

    package

---
+++ javamaster/mypackage/chap10.java
@@ -0,0 +1,29 @@
+//package com.javamaster.mypackage;
+//
+//public class chap10{
+//	String breed;
+//	String color;
+//	
+//	public void eat() {
+//		System.out.println("먹이를 먹다.");
+//	}
+//	public void scratch() {
+//		System.out.println("발톱으로 할퀴다.");
+//	}
+//	public void meow() {
+//		System.out.println("야옹하고 울다.");
+//	}
+//}
+package com.javamaster.mypackage;
+
+public class chap10{
+	String breed;
+	String color;
+	
+	public String bowwow() {
+		return "멍멍 짖다";
+	}
+	public void run() {
+		System.out.println("달리다");
+	}
+}
